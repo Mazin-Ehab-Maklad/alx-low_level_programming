ccf161f26ad53033e8d20dfc2989ed729942334e
Mazin-Ehab-Maklad <mazinmaklad4@gmail.com>
2023-07-21 02:04:48 +0300

    hi

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - bla bla bla
+ *@argc: bla
+ *@argv: bla
+ *
+ *Return: Nothing
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s", argv[argc - argc]);
+	return (0);
+}
