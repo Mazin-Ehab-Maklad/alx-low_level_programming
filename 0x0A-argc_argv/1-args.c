0ec4ccb618faa67efdb8fa2a1fac29e0b81ec7a2
Mazin-Ehab-Maklad <mazinmaklad4@gmail.com>
2023-07-21 15:28:31 +0300

    dina

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - bla
+ *@argc: bla
+ *@argv: bla
+ *
+ *Return: Nothing
+ */
+
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n",argc - 1);
+	return (0);
+}
